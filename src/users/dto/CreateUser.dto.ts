13eca77e98a45c46214924ceedbfed47329ff587
Jeova Junior <jrjeova@hotmail.com>
2023-01-28 16:46:51 -0300

    corrigindo

--- src/users/dto/CreateUser.dto.ts
+++ src/users/dto/CreateUser.dto.ts
@@ -2,7 +2,7 @@ import { IsString, IsEmail, MinLength, IsNotEmpty } from 'class-validator';
 import { EmailUnic } from '../validation/emailUnic.validator';
 
 export class CreateUserDTO {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  @IsNotEmpty({ message: 'O nome não pode ser vazio ' })
   name: string;
 
   // @EmailUnic({ message: 'Email ja Cadastrado' })
