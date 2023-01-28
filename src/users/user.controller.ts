3ab078cb9d782e48ce31e70b3fefcffa7147421f
Jeova Junior <jrjeova@hotmail.com>
2023-01-28 16:53:20 -0300

    resolvendo erro

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsePipes } from '@nestjs/common/decorators';
 import { ValidationPipe } from '@nestjs/common/pipes';
-import { CreateUserDTO } from './dto/createUser.dto';
+// import { CreateUserDTO } from './dto/createUser.dto';
 import { UserRepository } from './user.repository';
 
 @Controller('/users')
@@ -9,7 +9,7 @@ export class UserController {
   constructor(private userRepository: UserRepository) {}
   @UsePipes(new ValidationPipe({ transform: true }))
   @Post()
-  async createUser(@Body() request: CreateUserDTO) {
+  async createUser(@Body() request) {
     this.userRepository.save(request);
     return request;
   }
