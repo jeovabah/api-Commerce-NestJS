0974cfec295832780c1d394a0aa6d73fc39780c0
Jeova Junior <jrjeova@hotmail.com>
2023-02-16 21:37:40 -0300

    consertando colors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './users/user.module';
-import { ColorsModule } from './colors/colors.module';
 @Module({
   imports: [
     ProductsModule,
@@ -10,7 +9,6 @@ import { ColorsModule } from './colors/colors.module';
     MongooseModule.forRoot(
       'mongodb+srv://joaperi:34232114jeova@cluster0.v7ogoq4.mongodb.net/test',
     ),
-    ColorsModule,
   ],
   controllers: [],
   providers: [],
--- src/colors/dto/create-color.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateColorDto {
-  color_primary: string;
-  color_background: string;
-  color_text: string;
-}
